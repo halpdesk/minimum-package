1abda10b449e50bb3171c7ce0f835a7457a41dba
Daniel Leppänen <daniel.leppanen@gmail.com>
2019-06-26 13:03:12 +0200

    Added general exception class

---
+++ src/Exceptions/PackageException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Halpdesk\LaravelMinimumPackage\Exceptions;
+
+use Exception;
+use Halpdesk\LaravelMinimumPackage\Contracts\Exception as LaravelMinimumPackageExceptionContract;
+
+class PackageException extends Exception implements LaravelMinimumPackageExceptionContract {
+
+}
